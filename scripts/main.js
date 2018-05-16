e2d0074b58206054628c28377cbfa9cb5add0580
Joshua Jackson <washingtonfaulconer@gmail.com>
2018-05-16 15:15:56 +0100

    Removed Jquery (events and ajax) for vanilla js

--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,33 @@
+"use strict";
+
 const colours = ['#00A8FF', '#9C88FF', '#FBC531', '#4CD137', '#e84118', '#55efc4', '#81ecec',
     '#74b9ff', '#a29bfe', '#ffeaa7', '#fab1a0', '#ff7675', '#fd79a8', '#32ff7e',
     '#fff200', '#7efff5', '#C4E538', '#EE5A24', '#f78fb3', '#a4b0be', '#7bed9f', '#FC427B', '#F8EFBA'];
 
 let nouns = []; //The words grabbed from the wordnik api.
 
+/**DOM Elements*/
 const firstWord = document.getElementById("firstWord"); //The <span> for the first word.
 const secondWord = document.getElementById("secondWord"); //The <span> for the second word.
-
-let initialLoadComplete = false; //True if we still need to hide the loading icon/animation.
+const loadingIcon = document.getElementById("spinning-bottle");
+const pubNameText = document.getElementById("text"); //The whole text containing first/second word elements.
 
 let isSendingRequest = true; //True if we are waiting for an ajax request to return successfully.
 
-/*Grab the first collection of words from the wordnik api.*/
-grabNouns();
+/**Disables or Enables the loading icon.*/
+function showLoadingIcon(shouldShow)
+{
+    if (shouldShow)
+    {
+        loadingIcon.style.display = 'inline';
+        pubNameText.style.display = 'none';
+    }
+    else
+    {
+        loadingIcon.style.display = 'none';
+        pubNameText.style.display = 'inline';
+    }
+}
 
 /**Sends an ajax request to the wordnik api asking for 10 nouns of a limited length, we then store those 10 nouns
  * inside of the nouns[] array and, if this is first time this method has been called, we hide the spinning bottle
@@ -22,25 +37,56 @@ function grabNouns()
 {
     isSendingRequest = true;
 
-    $.ajax({
-        url: "http://api.wordnik.com/v4/words.json/randomWords?hasDictionaryDef=false&includePartOfSpeech=proper-noun&&minCorpusCount=10000&maxCorpusCount=-1&minDictionaryCount=3&maxDictionaryCount=-1&minLength=5&maxLength=15&limit=10&api_key=API_KEY", cache: false, success: function (result)
+    let wordnikAPIRequest = new XMLHttpRequest();
+
+    wordnikAPIRequest.onreadystatechange = function ()
+    {
+        if (this.readyState === XMLHttpRequest.DONE)
         {
-            for (let i = 0; i < 10; i++)
+            if (this.status === 200)
             {
-                nouns.push(result[i].word);
-            }
+                let result = JSON.parse(this.response);
 
-            if (!initialLoadComplete)
+                for (let i = 0; i < result.length; i++)
+                {
+                    nouns.push(result[i].word);
+                }
+
+                showLoadingIcon(false);
+
+                isSendingRequest = false;
+            }
+            else
             {
-                $("#spinning-bottle").hide();
-                $("#text").show();
-
-                initialLoadComplete = true;
+                console.error("An error occured with the request: " + this.status + " " + this.statusText);
+                showLoadingIcon(true);
+                alert("An error occured with the request: " + this.status + " " + this.statusText);
             }
-
-            isSendingRequest = false;
         }
-    });
+    }
+
+    //Setup Wordnik API Ajax request appending timestamp to avoid IE caching the request.
+    wordnikAPIRequest.open("GET", "http://api.wordnik.com/v4/words.json/randomWords?hasDictionaryDef=false&includePartOfSpeech=proper-noun&&minCorpusCount=10000&maxCorpusCount=-1&minDictionaryCount=3&maxDictionaryCount=-1&minLength=5&maxLength=15&limit=10&api_key=" + ((/\?/).test("http://api.wordnik.com/v4/words.json/randomWords?hasDictionaryDef=false&includePartOfSpeech=proper-noun&&minCorpusCount=10000&maxCorpusCount=-1&minDictionaryCount=3&maxDictionaryCount=-1&minLength=5&maxLength=15&limit=10&api_key=") ? "&" : "?") + (new Date()).getTime(), true);
+
+    showLoadingIcon(true);
+
+    wordnikAPIRequest.send();
+}
+
+/**Sends the chosen words to the DOM and then removes them from the nouns[] array*/
+function sendWordsToDOM(wordOne, wordTwo)
+{
+    /*Send the words to the DOM.*/
+    firstWord.innerText = wordOne;
+    secondWord.innerText = wordTwo;
+
+    /*Create new nouns array and remove the previous two words.*/
+    let temporaryNewNounArray = nouns;
+
+    temporaryNewNounArray.splice(temporaryNewNounArray.indexOf(wordOne), 1);
+    temporaryNewNounArray.splice(temporaryNewNounArray.indexOf(wordTwo), 1);
+
+    nouns = temporaryNewNounArray;
 }
 
 /**If we aren't waiting for a request to come back, pick 2 random words from the nouns[] array and
@@ -67,7 +113,7 @@ function generate()
         {
             wordIndexTwo = Math.floor(Math.random() * (nouns.length));
         }
-        while (wordIndexTwo == wordIndexOne)
+        while (wordIndexTwo === wordIndexOne)
 
         sendWordsToDOM(nouns[wordIndexOne], nouns[wordIndexTwo]);
 
@@ -75,28 +121,17 @@ function generate()
     }
 }
 
-/**Sends the chosen words to the DOM and then removes them from the nouns[] array*/
-function sendWordsToDOM(wordOne, wordTwo)
-{
-    /*Send the words to the DOM.*/
-    firstWord.innerText = wordOne;
-    secondWord.innerText = wordTwo;
 
-    /*Create new nouns array and remove the previous two words.*/
-    temporaryNewNounArray = nouns;
 
-    temporaryNewNounArray.splice(temporaryNewNounArray.indexOf(wordOne), 1);
-    temporaryNewNounArray.splice(temporaryNewNounArray.indexOf(wordTwo), 1);
-
-    nouns = temporaryNewNounArray;
-}
-
-/*Input checking.*/
 let keyIsDown = false;
 
-$(document).ready(function ()
+/**Setup Input Checking once the DOM is ready.*/
+document.addEventListener("DOMContentLoaded", function ()
 {
-    $(document).on("keydown", function (event)
+    grabNouns(); //Grab the initial collection of words from the wordnik api.
+
+    /*If SPACEBAR has been pressed and it isn't currently held down, generate a new word.*/
+    document.addEventListener("keydown", function (event)
     {
         if (event.keyCode === 32 && !keyIsDown)
         {
@@ -105,7 +140,7 @@ $(document).ready(function ()
         }
     });
 
-    $(document).on("keyup", function (event)
+    document.addEventListener("keyup", function (event)
     {
         if (event.keyCode === 32)
         {
@@ -113,5 +148,5 @@ $(document).ready(function ()
         }
     });
 
-    $(document).click(generate);
+    document.addEventListener("click", generate);
 });
